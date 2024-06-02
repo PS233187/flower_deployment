b7cb1d32fa3a092680fc1b9df7649c053c2537f5
jaszy-md <jaszydh@gmail.com>
2024-06-03 00:18:53 +0200

    Add build automation second

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:eindopdracht/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('HomePage displays welcome message', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MaterialApp(home: HomePage()));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our HomePage displays the welcome message.
+    expect(find.text('Welkom bij Jazzies Flowers!'), findsOneWidget);
+    expect(find.text('Ontdek de mooiste bloemen hier.'), findsOneWidget);
   });
 }
